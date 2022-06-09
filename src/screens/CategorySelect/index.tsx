5c307e520cda506c9fa72f45038fcad441631b66
otacilio.santos <otaciliogsantos@gmail.com>
2022-06-09 10:28:58 -0300

    chore: Creating screen Category and building modal

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import * as S from './styles'
+import { categories } from '../../mock'
+import { Button } from '../../components'
+
+export interface CategoryProps {
+    key: string
+    name: string
+    icon: string
+    color: string
+}
+
+interface CategoryCardProps {
+    item: CategoryProps
+}
+
+interface Props {
+    category: string
+    setCategory: (category: CategoryProps) => void
+    closeCategory: () => void
+}
+ 
+
+export const CategorySelect = ({
+    category,
+    setCategory,
+    closeCategory
+}: Props) => {
+
+    function CategoryCard ({ item }: CategoryCardProps) {
+        return (
+            <S.Category>
+                <S.Icon name={item.icon}/>
+                <S.Label>{item.name}</S.Label>
+            </S.Category>
+        )
+    }
+
+    return (
+        <S.Container>
+            <S.Header>
+                <S.Title>Categoria</S.Title>
+            </S.Header>
+            <S.CategoryList
+                data={categories}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => <CategoryCard  item={item}/>}
+                ItemSeparatorComponent={() => <S.Separator />}
+            />
+            <S.Footer>
+                <Button title="Selecionar"/>
+            </S.Footer>
+        </S.Container>
+    )
+}
---
+++ src/screens/CategorySelect/styles.ts
@@ -0,0 +1,67 @@
+import styled from 'styled-components/native'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { Feather } from '@expo/vector-icons'
+import { FlatList, FlatListProps } from 'react-native'
+import { CategoryProps } from '.'
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${({ theme }) => theme.colors.background};
+`
+
+export const Header = styled.View`
+    background-color: ${({ theme }) => theme.colors.primary};
+
+    height: ${RFValue(113)}px;
+    width: 100%;
+
+    align-items: center;
+    justify-content: flex-end;
+    padding-bottom: 19px;
+`
+
+export const Title = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular}; 
+    font-size: ${RFValue(18)}px;
+
+    color: ${({ theme }) => theme.colors.shape};
+`
+
+export const CategoryList = styled(
+    FlatList as new (props: FlatListProps<CategoryProps>) =>
+        FlatList<CategoryProps>
+)`
+    flex: 1;
+    width: 100%;
+`as unknown as typeof FlatList;
+
+export const Label = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular}; 
+    font-size: ${RFValue(14)}px;
+    color: ${({ theme }) => theme.colors.title};
+`
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(20)}px;
+    margin-right: 16px;
+`
+
+export const Category = styled.View`
+    width: 100%;
+    padding: ${RFValue(15)}px;
+
+    flex-direction: row;
+
+    align-items: center;
+`
+
+export const Separator = styled.View`
+    height: 1px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.text};
+`
+
+export const Footer = styled.View`
+    width: 100%;
+    padding: 24px;
+`
