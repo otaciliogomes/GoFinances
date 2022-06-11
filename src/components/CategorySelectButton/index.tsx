d9352106c15b75459722726bc996a67a69a82935
otacilio.santos <otaciliogsantos@gmail.com>
2022-06-11 16:24:43 -0300

    chore: Update components

--- src/components/CategorySelectButton/index.tsx
+++ src/components/CategorySelectButton/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { TouchableOpacityProps } from 'react-native'
 
 import * as S from './styles'
 
-interface Props {
+interface Props extends TouchableOpacityProps {
     title: string
 }
 
-export const CategorySelect = ({ title }: Props) => {
+export const CategorySelectButton = ({ title, ...rest }: Props) => {
     return (
-        <S.Container>
+        <S.Container {...rest}>
             <S.Title>{title}</S.Title>
             <S.Icon name="chevron-down"/>
         </S.Container>
